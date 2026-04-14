e2845e88b2ebc2d336a28edab2be562d1abc73a3
okliland-kodeloftet <okliland@hotmail.com>
2026-04-14 14:00:54 +0200

    ddd

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const submitButton = document.querySelector(#submitButton);
+const submitButton = document.querySelector("#submitButton");
 console.log(submitButton)
 
-submitButton.addeventListener("click", function() {
-    const todoInput = document.querySelector("#todoInput")
-    const todoList = document.querySelector("todoList")
+submitButton.addEventListener("click", function(){
+    const todoInput = document.querySelector("#todoInput").value
+    const todoList = document.querySelector("#todoList")
     console.log(todoInput , todoList)
 
 
     const newItem = document.createElement("li")
-    newItem.textContent = userInput
-    inputDisplay.appendChild(newItem);
+    newItem.textContent = todoInput
+    newItem.classList.toggle("todoItem")
+    const btn = document.createElement("button")
+    const ferdigBtn = btn
+    const slettBtn = btn
+    ferdigBtn.textContent("Ferdig")
+    slettBtn.textContent("Slett")
+    newItem.appendChild(btn);
+    todoList.appendChild(newItem);
+    event.preventDefault();
 })
